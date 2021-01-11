97e869b2f81c93781f1baf5c596e0fe29050ca11
moraruv0 <75850366+moraruv0@users.noreply.github.com>
2021-01-11 16:03:35 +0200

    Add files via upload

---
+++ inmultire.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <=10; i++) {
+    document.write("<br>")
+
+    document.write("<div>")
+    for (let j = 1; j <=10 ; j++) {
+        document.write(i + "x" + j + "=" + i*j + "<br>")
+    }
+    document.write("</div>")
+}
